01704547fc5432d513257f047aad7b57c43bdcd0
Martin <martinty@users.noreply.github.com>
2021-04-08 23:13:02 +0200

    Add RandomEngine class

---
+++ RandomEngine.h
@@ -0,0 +1,59 @@
+#pragma once
+
+// Inspired by CMB (Climbing Mont Blanc)
+// https://climb.idi.ntnu.no/#/howto
+//
+// Martin Tysseland - 08.04.2021
+// Class to generate random numbers using std::subtract_with_carry_engine
+// http://www.cplusplus.com/reference/random/subtract_with_carry_engine/
+// Examples:
+// {
+//     RandomEngine re;
+//     cout << "Random int [2, 7]: " << re.getRandInt(2, 7) << "\n";
+//     cout << "Random char ['a', 'e']: " << re.getRandChar('a', 'e') << "\n";
+//     cout << "Random number: " << re.rand() << "\n";
+// }
+
+#include <ctime>
+#include <random>
+
+namespace stdmt {
+
+class RandomEngine {
+   private:
+    std::subtract_with_carry_engine<unsigned int, 24, 10, 24> engine;
+    const unsigned int intRange;
+
+   public:
+    RandomEngine() : intRange{engine.max() - engine.min() + 1} {
+        engine.seed(static_cast<unsigned int>(time(nullptr)));
+    }
+    RandomEngine(unsigned int s) : intRange{engine.max() - engine.min() + 1} {
+        engine.seed(s);
+    }
+    RandomEngine(int s) : RandomEngine{static_cast<unsigned int>(s)} {}
+    ~RandomEngine() = default;
+    void setSeed(unsigned int s) { engine.seed(s); }
+    void discard(unsigned int n = 1) { engine.discard(n); }
+    unsigned int getMax() const { return engine.max(); }
+    unsigned int getMin() const { return engine.min(); }
+    unsigned int rand() { return engine(); }
+    char getRandChar(char from, char to) {
+        float fTmp = static_cast<float>(engine()) / intRange;
+        return (fTmp * (to - from + 1)) + from;
+    }
+    int getRandInt(int from, int to) {
+        float fTmp = static_cast<float>(engine()) / intRange;
+        return (fTmp * (to - from + 1)) + from;
+    }
+    float getRandFloat(float from, float to) {
+        float fTmp = static_cast<float>(engine()) / engine.max();
+        return (fTmp * (to - from)) + from;
+    }
+    double getRandDouble(double from, double to) {
+        float fTmp = static_cast<float>(engine()) / engine.max();
+        return (fTmp * (to - from)) + from;
+    }
+};
+
+}  // namespace stdmt
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 #include "ProgressBar.h"
+#include "RandomEngine.h"
 #include "TextColor.h"
 #include "Walltime.h"
 
@@ -24,4 +25,16 @@ int main() {
     }
     wt.stop();
     cout << wt << "\n";
+
+    // Testing random numbers
+    const int M = 5;
+    RandomEngine re;
+    for (int i = 0; i < M; i++) {
+        cout << "Random int [2, 7]: " << re.getRandInt(2, 7) << "\n";
+    }
+    for (int i = 0; i < M; i++) {
+        cout << "Random char ['a', 'e']: " << re.getRandChar('a', 'e') << "\n";
+    }
+
+    return 0;
 }
